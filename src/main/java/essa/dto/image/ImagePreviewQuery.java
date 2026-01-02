1d564ac24dcd9487967d41db7db9fbc73779eb36
nejcskrbec <71454677+nejcskrbec@users.noreply.github.com>
2026-01-02 22:11:28 +0100

    chore: revert

--- src/main/java/essa/dto/image/ImagePreviewQuery.java
+++ src/main/java/essa/dto/image/ImagePreviewQuery.java
@@ -12,16 +12,11 @@ public class ImagePreviewQuery {
 
     private List<String> tags;
 
-    public ImagePreviewQuery(
-            UUID id,
-            String bucketName,
-            String objectName,
-            Collection<String> tags
-    ) {
+    public ImagePreviewQuery(UUID id, String bucketName, String objectName, List<String> tags)  {
         this.id = id;
         this.bucketName = bucketName;
         this.objectName = objectName;
-        this.tags = tags == null ? List.of() : new ArrayList<>(tags);
+        this.tags = tags;
     }
 
     public UUID getId() {
