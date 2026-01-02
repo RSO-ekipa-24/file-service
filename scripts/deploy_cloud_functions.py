9d659f9bd1576d175a56c828ee8acd1450215763
Rozles <rozlesterle@gmail.com>
2026-01-02 23:29:39 +0100

    added internal load balancer with VPC access

--- scripts/deploy_cloud_functions.py
+++ scripts/deploy_cloud_functions.py
@@ -14,13 +14,13 @@ load_env_file(env_file=".env")
 
 gcp_project = os.getenv("GCP_PROJECT_ID")
 region = os.getenv("GCP_REGION")
+serverless_vpc_connector_name = os.getenv("GCP_SERVERLESS_VPC_CONNECTOR_NAME", "files-cloud-run-connector")
 private_bucket = os.getenv("GCP_PRIVATE_BUCKET")
 public_bucket = os.getenv("GCP_PUBLIC_BUCKET")
 kc_token_url = os.getenv("KC_TOKEN_URL")
 kc_client_id = os.getenv("KC_CLIENT_ID")
 kc_client_secret = os.getenv("KC_CLIENT_SECRET")
 confirm_file_service_url = os.getenv("CONFIRM_FILE_SERVICE_URL")
-
 
 # -------------------------
 # Detect gcloud/gsutil paths
@@ -39,6 +39,8 @@ subprocess.run(
         "eventarc.googleapis.com",
         "run.googleapis.com",
         "storage.googleapis.com",
+        "vpcaccess.googleapis.com",
+        "pubsub.googleapis.com",
         "--project", gcp_project
     ],
     check=True
@@ -88,6 +90,35 @@ subprocess.run(
 print()
 
 # -------------------------
+# Create Serverless VPC Access Connector - to access GKE services from Cloud Functions
+# -------------------------
+print("Create Serverless VPC Access Connector...")
+result = subprocess.run(
+    [
+        GCLOUD, "compute", "networks", "vpc-access", "connectors", "describe", serverless_vpc_connector_name,
+        "--region", region,
+        "--project", gcp_project
+    ],
+    capture_output=True,
+    text=True
+)
+
+if result.returncode != 0:
+    subprocess.run(
+        [
+            GCLOUD, "compute", "networks", "vpc-access", "connectors", "create", serverless_vpc_connector_name,
+            "--region", region,
+            "--network", "default",
+        "--range", "10.0.0.0/28",
+    ],
+    check=True
+)
+else:
+    print(f"Serverless VPC Access Connector '{serverless_vpc_connector_name}' already exists.")
+    
+print()
+
+# -------------------------
 # Deploy confirm-upload Cloud Functions
 # -------------------------
 print("Deploy confirm-upload-private Cloud Function...")
@@ -101,6 +132,8 @@ subprocess.run(
         "--entry-point", "on_object_finalize",
         "--trigger-event", "google.cloud.storage.object.v1.finalized",
         "--trigger-resource", private_bucket,
+        "--vpc-connector", serverless_vpc_connector_name,
+        "--egress-settings", "private-ranges-only",
         "--set-env-vars",
         f"KC_TOKEN_URL={kc_token_url},KC_CLIENT_ID={kc_client_id},KC_CLIENT_SECRET={kc_client_secret},CONFIRM_FILE_SERVICE_URL={confirm_file_service_url}",
         "--project", gcp_project
@@ -120,6 +153,8 @@ subprocess.run(
         "--entry-point", "on_object_finalize",
         "--trigger-event", "google.cloud.storage.object.v1.finalized",
         "--trigger-resource", public_bucket,
+        "--vpc-connector", serverless_vpc_connector_name,
+        "--egress-settings", "private-ranges-only",
         "--set-env-vars",
         f"KC_TOKEN_URL={kc_token_url},KC_CLIENT_ID={kc_client_id},KC_CLIENT_SECRET={kc_client_secret},CONFIRM_FILE_SERVICE_URL={confirm_file_service_url}",
         "--project", gcp_project
